fd95b9d9670e18c2e19d6a535aca83654b46b7af
Sarthak Gupta <sarthakg4953@gmail.com>
2024-06-10 20:40:19 +0530

    Initial Commit

---
+++ tests/test_cyphertext.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `cyphertext` package."""
+
+
+import unittest
+
+from cyphertext import cyphertext
+
+
+class TestCyphertext(unittest.TestCase):
+    """Tests for `cyphertext` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
